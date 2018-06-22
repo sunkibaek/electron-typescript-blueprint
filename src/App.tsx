5a0e11ef72f272cb298b20a26545b7d67bfeba58
Sunki Baek <sunki.baek@gmail.com>
2018-06-22 09:35:39 +0900

    Add blueprint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Intent, Spinner } from "@blueprintjs/core";
 import React from "react";
 import ReactDOM from "react-dom";
 
 import Title from "./Title";
 
-const App = () => <Title />;
+const App = () => (
+  <div>
+    <Title />
+    <Spinner intent={Intent.PRIMARY} />
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
